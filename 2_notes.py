1ef214e7942ac2b789c82bb9dfb696adaf3ba0fc
Gabrielle Oliva <gabrielle.oliva55@gc.cuny.edu>
2026-09-08 10:34:28 -0400

    work from class day 3

---
+++ 2_notes.py
@@ -0,0 +1,5 @@
+# Notes from Day 2
+
+# I was sick
+
+# get notes from someone else
---
+++ HW_1.py
@@ -0,0 +1,5 @@
+#  HW 1 Sample Problem from Website
+# DUE 9/15/26
+###  Write a program that calculates the time it takes for a ball to drop from a user specified
+### height to reach the ground. Use argparse. Also the user to choose different values of gravity. 
+### And any other features you think maybe interesting.
